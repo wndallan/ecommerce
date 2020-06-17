dea8245831c70635b038856e53e1266be0b0c617
wndallan <wndallan1@gmail.com>
2020-06-17 09:33:43 -0300

    Aula 8

---
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Hcode;
+
+use PHPMailer;
+use Rain\Tpl;
+
+class Mailer {
+
+    const USERNAME = "wndstudent@gmail.com";
+    const PASSWORD = "apenasteste1415";
+    const NAME_FROM = "Hcode Store";
+
+    private $mail;
+
+    public function __construct($toAddress, $toName, $subject, $tlpName, $data = []){
+      
+        $config = [
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/email/",
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+            "debug"         => false
+        ];
+
+        Tpl::configure($config);
+
+        $tpl = new Tpl;
+
+        foreach ($data as $key => $value) {
+            $tpl->assign($key, $value);
+        }
+
+        $html = $tpl->draw($tlpName, true);
+
+        $this->mail = new \PHPMailer();
+        $this->mail->isSMTP();
+        $this->mail->SMTPDebug = 0;
+        $this->mail->Debugoutput = 'html';
+        $this->mail->Host = 'smtp.gmail.com';
+        $this->mail->Port = 587;
+        $this->mail->SMTPSecure = 'tls';
+        $this->mail->SMTPAuth = true;
+        $this->mail->Username = Mailer::USERNAME;
+        $this->mail->Password = Mailer::PASSWORD;
+        $this->mail->setFrom(Mailer::USERNAME, Mailer::NAME_FROM);
+        $this->mail->addAddress($toAddress, $toName);
+        $this->mail->Subject = $subject;
+        $this->mail->msgHTML($html);
+        $this->mail->AltBody = 'body';
+
+    }
+
+    public function send(){
+
+        return $this->mail->send();
+
+    }
+
+}
+
+?>
