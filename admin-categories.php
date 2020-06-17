5ffe2fad7e2404613b7b8739136ae5ff5e68a723
wndallan <wndallan1@gmail.com>
2020-06-17 14:22:53 -0300

    Aula 11

---
+++ admin-categories.php
@@ -0,0 +1,96 @@
+<?php
+
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+use \Hcode\Model\Category;
+
+$app->get("/admin/categories/", function(){
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		"categories" => $categories
+	]);
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get('/admin/categories/create', function() {
+
+	User::verifyLogin();
+    
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->get('/admin/categories/:idcategory', function($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+    
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		"category" => $category->getValues()
+	]);
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+?>
---
+++ site.php
@@ -0,0 +1,32 @@
+<?php
+
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+use \Hcode\Model\Category;
+
+$app->get('/', function() {
+    
+	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get("/categories/:idcategory", function($idcategory){
+	
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		"category" => $category->getValues(),
+		"products" => []
+	]);
+
+});
+
+?>
