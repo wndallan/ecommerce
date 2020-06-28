134132b2cf554a3c7b6dc2d2645e9c57ad5fccbf
wndallan <57689606+wndallan@users.noreply.github.com>
2020-06-28 11:24:56 -0300

    Aula 30

---
+++ SQL/sp_users_delete.sql
@@ -0,0 +1,31 @@
+DROP PROCEDURE IF EXISTS `sp_users_delete`;
+
+DELIMITER $$
+CREATE PROCEDURE `sp_users_delete`(
+piduser INT
+)
+BEGIN
+    
+    DECLARE vidperson INT;
+    
+    SET FOREIGN_KEY_CHECKS = 0;
+	
+	SELECT idperson INTO vidperson
+    FROM tb_users
+    WHERE iduser = piduser;
+	
+    DELETE FROM tb_addresses WHERE idperson = vidperson;
+    DELETE FROM tb_addresses WHERE idaddress IN(SELECT idaddress FROM tb_orders WHERE iduser = piduser);
+	DELETE FROM tb_persons WHERE idperson = vidperson;
+    
+    DELETE FROM tb_userslogs WHERE iduser = piduser;
+    DELETE FROM tb_userspasswordsrecoveries WHERE iduser = piduser;
+    DELETE FROM tb_orders WHERE iduser = piduser;
+    DELETE FROM tb_cartsproducts WHERE idcart IN(SELECT idcart FROM tb_carts WHERE iduser = piduser);
+    DELETE FROM tb_carts WHERE iduser = piduser;
+    DELETE FROM tb_users WHERE iduser = piduser;
+    
+    SET FOREIGN_KEY_CHECKS = 1;
+    
+END$$
+DELIMITER ;
